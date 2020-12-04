9745c8fc7f9fd65674de85964fc47431e66cc356
abdcelik <acelik29@outlook.com>
2020-12-04 18:41:10 +0300

    models(basket, person, user)

---
+++ src/models/Basket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fusion
+{
+    class Basket
+    {
+        public List<Book> book { get; private set; }
+        public int totalPrice { get; set; }
+
+        public Basket() { book = new List<Book>(0); }
+
+        public void addBook(Book book)
+        {
+            this.book.Add(book);
+            totalPrice += book.price;
+        }
+
+        public bool removeBook(Book book)
+        {
+            bool res;
+
+            if ((res = this.book.Remove(book)))
+                totalPrice -= book.price;
+            return res;
+        }
+    }
+}
---
+++ src/models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fusion
+{
+    class Person
+    {
+        public String name { get; set; }
+        public String surname { get; set; }
+        public int ID { get; }
+
+        public Person(String name, String surname, int ID)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.ID = ID;
+        }
+
+        public override string ToString() { return String.Format("Name: {0}\nSurname: {1}\nID: {2}\n", name, surname, ID); }
+    }
+}
