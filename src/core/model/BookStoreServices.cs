a193a5c53488ff5505a863164c98f8b8bccf54dc
BerkP <45625829+BerkP@users.noreply.github.com>
2020-12-27 16:32:04 +0300

    Service Class Added and DB Class Updated

---
+++ src/core/model/BookStoreServices.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FusionBookieDB.Entity;
+
+namespace FusionBookieDB
+{
+    public class BookStoreServices
+    {
+
+        FusionDb Db = new FusionDb();
+
+        public static User Login()
+        {
+            //TODO
+
+            return null;
+        }
+
+
+        public static User SignUp()
+        {
+            //TODO
+
+            return null;
+        }
+
+        public static List<Category> GetCategories()
+        {
+            //TODO
+
+            return null;
+        }
+
+        public static List<Book> Filter()
+        {
+            //TODO
+
+            return null;
+        }
+
+
+
+
+
+    }
+}
--- src/core/model/Program.cs
+++ src/core/model/Program.cs
@@ -3,46 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Fusion.Entity;
+using FusionBookieDB.Entity;
 
-namespace Fusion
+namespace FusionBookieDB
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*
             User currUser;
             Book currBook;
             FusionDb db = new FusionDb();
 
-            db.Users.Add(new User {
-                Username = "Chae",
-                Password = "6789",
-                Address  = "Ofis"
 
-            });
+            int asd = 15;
+            var filts = db.Books.Where(x => asd % x.FilterID == 0 ).ToList();
+
+            /*User myUser = new User("chae", "1243", "berk", "pkgz");
+            myUser.PhoneNumber = "4321";
+            myUser.Address = "xd";
+            Cart myCart = new Cart();
+            myCart.Price = 9;
+            myUser.Cart = myCart;
+
+            db.Users.Add(myUser);
             db.SaveChanges();*/
 
-            /*
-            var data = db.Users.Where(x => x.Username == "Norius").ToList();
+            var data = db.Users.Where(x => x.UserName == "chae").ToList();
             if (data.Count == 1)
             {
-                currUser = data.FirstOrDefault();
-                Console.WriteLine(currUser.Cart.Price);
-                
+                var data2 = db.Categories.ToList();
+                List<Category> Cats = data2;
+
+                for(int i=0; i < Cats.Count; i++)
+                {
+                    Console.WriteLine(Cats[i].FilterID);
+                }
+
+                /*currBook = currUser.CurrSales[3];
+                Console.WriteLine(currBook.Tags[0].FilterID);*/
+
+
+                /*currBook = new Book();
+                currBook.Name = "Olasiliksiz4";
+                currBook.Price = 10;
+                currBook.Author = "Apo";
+                currBook.Edition = "Founder Edition";
+                currBook.Page = 200;
+                currBook.PhotoUrl = "/imgs/4";
+                currBook.FilterID = 0;
+                currBook.User = currUser;
+
+                currUser.CurrSales.Add(currBook);*/
+
+                /*currBook = currUser.CurrSales[3];
+                currBook.FilterID = 6;
+                Category newC = new Category();
+                newC.Name = "Peotry";
+                newC.FilterID = 43;
+                currBook.Tags.Add(newC);*/
+
+                //db.SaveChanges();
+                //Console.WriteLine(currUser.CurrSales[0].Name);
+
+
+                //Console.WriteLine(Tag.Art_Photography.ToString().Replace('_',' '));
+               // Console.WriteLine(((Tag)3).ToString());
+               // Console.WriteLine(currUser.Password);
             }
-            */
 
-           /* var data = db.Books.Where(x => x.Name == "berk kitap").ToList();
-            if (data.Count == 1)
-            {
-                currBook = data.FirstOrDefault();
-                Console.WriteLine(currBook.Name);
-                currUser = currBook.User;
-                Console.WriteLine(currUser.Address);
 
-            }*/
+
         }
     }
 }
